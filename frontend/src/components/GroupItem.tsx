c8a970b4efeb8c9fb790d3d579c0a4dad583a604
ntindle <nicktindle@outlook.com>
2021-10-02 02:00:41 -0500

    feat: add disappointmetn

---
+++ frontend/src/components/GroupItem.tsx
@@ -0,0 +1,191 @@
+import React from "react";
+import { useHistory } from "react-router-dom";
+import {
+  AppBar,
+  Avatar,
+  Button,
+  IconButton,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemIcon,
+  ListItemText,
+  makeStyles,
+  Toolbar,
+  Typography,
+} from "@material-ui/core";
+import MenuIcon from "@material-ui/icons/Menu";
+
+const useStyles = makeStyles({
+  wrapper: {
+    display: "flex",
+    justifyContent: "space-around",
+    border: "6px solid #e0e0e0",
+    borderRadius: "45px",
+  },
+  groupInfoWrapper: {
+    display: "flex",
+    alignItems: "center",
+    flexDirection: "column",
+    width: "30%",
+  },
+  groupTitle: {
+    display: "flex",
+  },
+  groupDescription: {
+      display: "flex",
+  },
+  groupMemberCount: {
+    display: "flex",
+  },
+});
+
+export interface Group {
+  groupName: string;
+  groupDescription: string;
+  members: string[];
+  guid: string;
+  schedule: Event[];
+  channels: Channel[];
+}
+
+export interface Channel {
+  channelName: string;
+  channelDescription: string;
+  guid: string;
+  posts: Post[];
+}
+
+export interface Post {
+  channelId: string;
+  title?: string;
+  date: Date;
+  content: string;
+  mediaLink: string;
+  guid: string;
+}
+
+export interface Event {
+  name: string;
+  date: Date;
+  duration: number;
+  details: string;
+  location: string;
+  participants: string[];
+  organizers: string[];
+  guid: string;
+}
+
+export interface Interest {
+  interestName: string;
+  guid: string;
+}
+
+export interface Member {
+  name: string;
+  guid: string;
+  email: string;
+  username: string;
+  interests: string[];
+  picture?: string;
+  status: string;
+  groups: string[];
+}
+
+export interface Notification {
+  message: string;
+  type: string;
+  guid: string;
+}
+const GetMemberById = (memberId: string): Member => {
+  const members = [
+    {
+      name: "John Doe",
+      guid: "123",
+      email: "johnDoe@gmail.com",
+      username: "johnDoe",
+      interests: ["Coding", "Reading"],
+      picture: "",
+      status: "Active",
+      groups: ["123", "456"],
+    },
+    {
+      name: "Jane Doe",
+      guid: "456",
+      email: "janedoe@gmail.com",
+      username: "janedoe",
+      interests: ["Coding", "Reading"],
+      picture: "",
+      status: "Active",
+      groups: ["123", "456"],
+    },
+  ];
+  return (
+    members.find((x) => x.guid == memberId) || {
+      name: "",
+      guid: "",
+      email: "",
+      username: "",
+      interests: [],
+      picture: "",
+      status: "",
+      groups: [],
+    }
+  );
+};
+
+interface Props {
+  group: Group;
+}
+const GroupItem = (props: Props): JSX.Element => {
+  const classes = useStyles();
+  const group = props.group;
+  const members = group.members.map((member: string) => GetMemberById(member));
+  return (
+    <div className={classes.wrapper}>
+      <>
+        <List className={classes.groupInfoWrapper}>
+          <ListItemText
+            primary={
+              <React.Fragment>
+                <Typography variant="h4">{group.groupName}</Typography>
+              </React.Fragment>
+            }
+            secondary={
+              <React.Fragment>
+                <Typography
+                  className={classes.groupDescription}
+                  variant="body1"
+                >
+                  {group.groupDescription}
+                </Typography>
+              </React.Fragment>
+            }
+          />
+        </List>
+        <List>
+          {members.map((member) => {
+            return (
+              <ListItem key={member.guid}>
+                <ListItemAvatar>
+                  <Avatar src={member.picture || "/static/unknown-image.png"} />
+                </ListItemAvatar>
+                <ListItemText primary={member.name} />
+              </ListItem>
+            );
+          })}
+        </List>
+        <List>
+          {group.schedule.map((event) => {
+            return (
+              <ListItem key={event.guid}>
+                <ListItemText primary={event.name} />
+              </ListItem>
+            );
+          })}
+        </List>
+      </>
+    </div>
+  );
+};
+export default GroupItem;
--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,6 +1,7 @@
-import React from "react"
-import { makeStyles, Typography } from "@material-ui/core"
-import home from "../assets/home.svg"
+import React from "react";
+import { makeStyles, Typography } from "@material-ui/core";
+import home from "../assets/home.svg";
+import GroupItem, { Group } from "../components/GroupItem";
 
 // Styles
 const useStyles = makeStyles({
@@ -11,16 +12,70 @@ const useStyles = makeStyles({
     justifyContent: "space-around",
     alignItems: "center",
   },
-})
+  groups: {
+    width: "90%",
+    margin: '10px',
+    alignItems: "center",
+  },
+});
 
 // HomePage coponent
 const HomePage = (): JSX.Element => {
-  const classes = useStyles()
+  const classes = useStyles();
+  const groups: Group[] = [
+    {
+      groupName: "Bipolar Sunshine",
+      groupDescription: "Friends of those who are a bit bipolar",
+      members: ["123", "456"],
+      guid: "123",
+      schedule: [
+        {
+          duration: 60,
+          date: new Date(2020, 1, 1),
+          name: "Happy Meeting Workshop",
+          location: "Somewhere",
+          details: "We will code",
+          guid: "178",
+          organizers: ["123"],
+          participants: ["456"],
+        },
+      ],
+      channels: [],
+    },
+    {
+      groupName: "Subtract ADD",
+      groupDescription: "Come hang out with us, we are a bit around",
+      members: ["123", "456"],
+      guid: "123",
+      schedule: [
+        {
+          duration: 60,
+          date: new Date(2020, 1, 1),
+          name: "Happy Meeting Workshop",
+          location: "Somewhere",
+          details: "We will code",
+          guid: "178",
+          organizers: ["123"],
+          participants: ["456"],
+        },
+      ],
+      channels: [],
+    },
+  ];
   return (
-    <div className={classes.root}>
-      <Typography variant="h1" component="h1">Welcome Home</Typography>
-      <img src={home} />
-    </div>
-  )
-}
-export default HomePage
+    <React.Fragment>
+      <div className={classes.root}>
+        <Typography variant="h1" component="h1">
+          Welcome Home
+        </Typography>
+        <img src={home} />
+      </div>
+      {groups.map((group) => (
+        <div className={classes.groups}>
+          <GroupItem group={group} />
+        </div>
+      ))}
+    </React.Fragment>
+  );
+};
+export default HomePage;
