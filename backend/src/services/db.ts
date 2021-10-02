e1cac89752a9c14be0c529ec75efcf4fb1732304
Sebastian King <sebastian.king@btinternet.com>
2021-10-02 04:09:27 -0500

    fix mongo connection, add env, remove yarn

--- backend/src/services/db.ts
+++ backend/src/services/db.ts
@@ -4,22 +4,19 @@ import { Collection, MongoClient } from "mongodb"
  * create a reference to the mongodb database
  */
 const useMongoDB = async (): Promise<MongoClient> => {
-  return new Promise((resolve, reject) => {
-    MongoClient.connect("mongodb://localhost:27017", (err, client) => {
-      if (!!err) {
-        return reject(err)
-      } else {
-        return resolve(client)
-      }
-    })
-  })
+  const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@melonmentor.ghwuy.mongodb.net/MelonMentor?retryWrites=true&w=majority`;
+
+  console.debug("Connecting to MongoDB using username:", process.env.MONGO_USER);
+
+  const client = new MongoClient(uri);
+  return client.connect();
 }
 
 /**
  * create a connection to a specific collection in mongodb
  */
 export const useCollection = async <T>(collectionName: string): Promise<Collection<T>> => {
-  const client = await useMongoDB()
-  const db = await client.db("hackathon-starter")
-  return await db.collection<T>(collectionName)
+  const client = await useMongoDB();
+  const db = await client.db("MelonMentor");
+  return await db.collection<T>(collectionName);
 }
