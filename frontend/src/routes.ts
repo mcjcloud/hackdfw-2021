b92571cd737d5a6d45847844d085376a7911f2f7
annakila-myndvr <88678565+annakila-myndvr@users.noreply.github.com>
2021-10-03 06:35:29 -0500

    routing fix for completed profile page

--- frontend/src/routes.ts
+++ frontend/src/routes.ts
@@ -4,12 +4,14 @@ import HomePage from "./pages/HomePage";
 import Signup from "./pages/SignupPage/SignupPage";
 import Profile from "./pages/ProfilePage/ProfilePage";
 import GroupPage from "./pages/GroupPage";
+import ProfilePage from "./pages/ProfilePage";
 import Matches from "./pages/MatchesPage/MatchesPage";
 
 const routes: RouteProps[] = [
   { path: "/", component: HomePage, exact: true },
   { path: "/signup", component: Signup, exact: true },
   { path: "/profile", component: Profile, exact: true },
+  { path: "/me", component: ProfilePage, exact: true},
   { path: "/matches", component: Matches, exact: true },
   { path: "/group/:id", component: GroupPage, exact: true },
 ];
