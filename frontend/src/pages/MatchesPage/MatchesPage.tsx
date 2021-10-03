ccfba580f365215837187925fbbfa2dc938462bb
Sebastian King <sebastian.king@btinternet.com>
2021-10-03 06:29:42 -0500

    matcher v1

--- frontend/src/pages/MatchesPage/MatchesPage.tsx
+++ frontend/src/pages/MatchesPage/MatchesPage.tsx
@@ -6,6 +6,12 @@ import Radio from '@material-ui/core/Radio';
 import React from "react";
 import { RouteComponentProps, StaticContext } from "react-router";
 import Wizard, { WizardStep } from "../../components/Wizard/Wizard";
+import "./MatchesPage.scss";
+import avatar1 from "../../assets/avatars/avatar1.png";
+import avatar2 from "../../assets/avatars/avatar2.png";
+import avatar3 from "../../assets/avatars/avatar3.png";
+import avatar4 from "../../assets/avatars/avatar4.png";
+import avatar5 from "../../assets/avatars/avatar5.png";
 
 const Matches = (): JSX.Element => {
     const paperStyle = { padding: '40px 40px', width: 350, margin: "30px auto" };
@@ -15,11 +21,70 @@ const Matches = (): JSX.Element => {
         <>
             <Wizard step={WizardStep.MATCHES}/>
 
-            <Grid>
-                <Paper elevation={20} style={paperStyle}>
-                </Paper>
+            <div className="matches-container">
+                <div className="match">
+                    <div className="avatar-section"><img src="https://smb.ibsrv.net/imageresizer/image/article_manager/1200x1200/158881/238442/heroimage0.172831001604055122.jpg" /></div>
+                    <div className="info">
+                        <div className="top-row">
+                            <Typography className="inline-heading" variant="h6">Threshold of Hope</Typography>
+                            <span className="heading-caption">
+                                <small>
+                                &nbsp;— Group since Jan 16, 2020
+                                </small>
+                            </span>
+                            <span className="top-row-right">
+                                <Button size="small" variant="outlined">Preview</Button>
+                                &nbsp;&nbsp;&nbsp;
+                                <Button size="small" variant="contained" color='primary'>Join</Button>
+                            </span>
+                        </div>
+                        <div>
+                            <span className="description-section">
+                                <div className="main">
+                                    The Threshold of Hope group offers online support groups, chat rooms, and forums for members to share their feelings, provide daily check-ins, discuss their experiences with depression, and share encouragement and support.
+                                </div>
+                                <div className="sec">
+                                    <span className="highlighted-person">
+                                        <img src={avatar1} />
+                                        <p><strong>Rob</strong><br />James</p>
+                                        <small className="qual">TPSA</small>
+                                    </span>
+                                    <span className="highlighted-person">
+                                        <img src={avatar5} />
+                                        <p><strong>Hugh</strong><br />Evans</p>
+                                        <small className="qual">Counsellor</small>
+                                    </span>
+                                    <span className="highlighted-person">
+                                        <img src={avatar4} />
+                                        <p><strong>Priy</strong><br />Vardy</p>
+                                        <small className="qual">S.W.</small>
+                                    </span>
+                                    <span className="highlighted-person">
+                                        <img src={avatar3} />
+                                        <p><strong>Rohit</strong><br />Dhoy</p>
+                                        <small className="qual">TPSA</small>
+                                    </span>
+                                </div>
+                            </span>
+                            <span className="extra-section">
+                                <h4>Scheduled Events/Meetups</h4>
+                                <ul>
+                                    <li>Tea &amp; Biscuits - 4:30pm Wed.</li>
+                                    <li>Group Mentor - 7pm Sat.</li>
+                                </ul>
 
-            </Grid>
+                                <p className="right-align"><small>66 members</small></p>
+                            </span>
+                        </div>
+                    </div>
+                </div>
+                <div className="match">
+                    test
+                </div>
+                <div className="match">
+                    test
+                </div>
+            </div>
         </>
     )
 }
